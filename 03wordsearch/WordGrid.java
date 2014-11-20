cc477f6053f334ec01b8a462f025333e4b5184d4
Cookie <mingrui.li3@gmail.com>
2014-11-19 22:35:47 -0500

    finished up toString, started addWordHorizontal, will finish tomorrow morning

--- 03wordsearch/WordGrid.java
+++ 03wordsearch/WordGrid.java
@@ -1,5 +1,5 @@
 public class WordGrid{
-    private char[][]data;
+    private char[][] data;
     private int rowcounter, colcounter;
     
     /**Initialize the grid to the size specified and fill all of the positions
@@ -7,8 +7,8 @@ public class WordGrid{
      *@param row is the starting height of the WordGrid
      *@param col is the starting width of the WordGrid
      */
-    public WorldGrid(int rows, int cols){
-	data = data[rows][cols];
+    public WordGrid(int rows, int cols){
+        data = new char[rows][cols];
 	rowcounter = rows;
 	colcounter = cols;
     }
@@ -19,10 +19,11 @@ public class WordGrid{
 	while (counter != 0){
 	    int counter2 = colcounter;
 	    while (counter2 != 0){
-		data[counter][counter2] = " ";
+		String s = " ";
+		data[counter][counter2] = s.charAt(0);
 		counter2 = counter2 - 1;
 	    }
-	    counter1 = counter1 - 1;
+	    counter = counter - 1;
 	}
     }
 
@@ -31,8 +32,21 @@ public class WordGrid{
      *separated by newlines.
      */
     public String toString(){
+	String printing = "";
+	int counter = 0;
+	while(counter < rowcounter){
+	    int counter2 = 0;
+	    while (counter2 < colcounter){
+		if (counter2 == rowcounter - 1){
+		    printing = printing + data[counter][counter2] + "\n";
+		}
+		printing = printing + data[counter][counter2];
+		counter2 = counter2 + 1;
+	    }
+	    counter = counter + 1;
+	}
+	return printing;
     }
-    
     /**Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from left to right, must fit on the WordGrid, and must
      *have a corresponding letter to match any letters that it overlaps.
@@ -44,6 +58,10 @@ public class WordGrid{
      *or there are overlapping letters that do not match, then false is returned.
      */
     public boolean addWordHorizontal(String word,int row, int col){
+	if (word.length() < (colcounter - col)){
+	    int counter = word.length();
+	}
+	return false;
     }
 
     //vertical + diagonal should be implemented as well.

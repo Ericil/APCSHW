d91366d1a9e6e85a87fae4e7af805f3bf82c248a
Cookie <mingrui.li3@gmail.com>
2014-11-21 12:23:43 -0500

    helping code now works for 3 directions, will test the other 5 directions later. Should work for them though

--- 03wordsearch/WordGrid.java
+++ 03wordsearch/WordGrid.java
@@ -49,57 +49,97 @@ public class WordGrid{
 	}
 	return printing;
     }
-    //rowdirec: 1 is to theright, -1 is to the left, 0 is no movement in that direction
+    //rowdirec: 1 is to the right, -1 is to the left, 0 is no movement in that direction
     //coldirect: 1 is down, -1 is up, 0 is no movement in that direction
-    public boolean helping(String word, int row, int col, int rowdirec, int coldirec){
-	if ((rowadd <= (rowcounter - row)) && (coladd <= (colcounter - col))){
-	    int counter = 0;
-	    int rows = 0;
-	    int cols = 0;
-	    if (rowdirect == 1){
+    public boolean helping(String word, int row, int col, int rowdirect, int coldirect){
+	int rows = 0;
+	int cols = 0;
+	if (rowdirect == 1){
+	    if ((row + word.length() - 1) <= rowcounter){
 		rows = word.length();
-	    }else if (rowdirect == -1){
+	    }else{
+		System.out.println("1");
+		return false;
+	    }
+	}else if (rowdirect == -1){
+	    if ((row - word.length() + 1) >= 0){
 		rows = word.length();
-		int reversecounter = word.length - 1;
-		String newword = "";
+		row = row - word.length() + 1;
+		int reversecounter = word.length() - 1;
+		String newword = ""; 
 		while (reversecounter >= 0){
 		    newword = newword + word.charAt(reversecounter);
 		    reversecounter = reversecounter - 1;
 		}
 		word = newword;
+	    }else{
+		System.out.println("2");
+		return false;
 	    }
+	}
+	if (coldirect == 1){
+	    if ((col + word.length() - 1) <= colcounter){
+		cols = word.length();
+	    }else{
+		System.out.println("3");
+		return false;
+	    }
+	}else if (coldirect == -1){
+	    if ((col - word.length() + 1) >= 0){
+		cols = word.length();
+		col = col - word.length() + 1;
+		int reversecounter = word.length() - 1;
+		String newword = ""; 
+		while (reversecounter >= 0){
+		    newword = newword + word.charAt(reversecounter);
+		    reversecounter = reversecounter - 1;
+		}
+		word = newword;
+	    }else{
+		System.out.println("4");
+		return false;
+	    }
+	}
+	System.out.println("Rows: " + rows + ", Cols: " + cols);
+	if ((rows <= (rowcounter - row)) && (cols <= (colcounter - col))){
+	    int counter = 0;
+	    int rowscounter = rows;
+	    int colscounter = cols;
 	    boolean cookies = false;
 	    while ((counter < word.length() - 1) && cookies == false){
-		if ((data[row + (rowadd - rows)][col + (coladd - cols)] == s.charAt(0)) || (data[row + (rowadd - rows)][col + (coladd - cols)] == word.charAt(counter))){
+		if ((data[row + (rows - rowscounter)][col + (cols - colscounter)] == s.charAt(0)) || (data[row + (rows - rowscounter)][col + (cols - colscounter)] == word.charAt(counter))){
 		    counter = counter + 1;
-		    if (rows != 0){
-			rows = rows - 1;
+		    if (rowscounter != 0){
+			rowscounter = rowscounter - 1;
 		    }
-		    if (cols != 0){
-			cols = cols - 1;
+		    if (colscounter != 0){
+			colscounter = colscounter - 1;
 		    }
 		}else{
 		    cookies = true;
 		}
 	    }
 	    if (cookies == true){
+		System.out.println("5");
 		return false;
 	    }
 	    counter = 0;
-	    rows = rowadd;
-	    cols = coladd;
+	    rowscounter = rows;
+	    colscounter = cols;
 	    while (counter < word.length() - 1){
-		data[row + (rowadd - rows)][col + (coladd - cols)] = word.charAt(counter);
-		if (rows != 0){
-		    rows = rows - 1;
+		data[row + (rows - rowscounter)][col + (cols - colscounter)] = word.charAt(counter);
+		if (rowscounter != 0){
+		    rowscounter = rowscounter - 1;
 		}
-		if (cols != 0){
-		    cols = cols - 1;
+		if (colscounter != 0){
+		    colscounter = colscounter - 1;
 		}
 		counter = counter + 1;
 	    }
+	    System.out.println("6");
 	    return true;
 	}
+	System.out.println("7");
 	return false;
     }
 	
@@ -114,7 +154,7 @@ public class WordGrid{
      *or there are overlapping letters that do not match, then false is returned.
      */
     public boolean addWordHorizontal(String word,int row, int col){
-	return helping(word, row, col, 0, word.length());
+	return helping(word, row, col, 0, 1);
     }
     /**Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from top to bottom, must fit on the WordGrid, and must
@@ -127,7 +167,7 @@ public class WordGrid{
      *or there are overlapping letters that do not match, then false is returned.
      */
     public boolean addWordVertical(String word, int row, int col){
-	return helping(word, row, col, word.length(), 0);
+	return helping(word, row, col, 1, 0);
     }
     /**Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from left to right,top to bottom, must fit on the WordGrid, and must
@@ -140,7 +180,7 @@ public class WordGrid{
      *or there are overlapping letters that do not match, then false is returned.
      */
     public boolean addWordDiagonal(String word, int row, int col){
-	return helping(word, row, col, word.length(), word.length());
+	return helping(word, row, col, 1, 1);
     }
     public static void main(String[]args){
 	WordGrid tester = new WordGrid(10, 10);
